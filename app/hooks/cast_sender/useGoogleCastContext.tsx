607601b1f2ada297c62ec66d58c1c5a2941ee8d4
michaelblawrence <michael@wondering.com>
2024-04-04 00:53:58 +0100

    add load request message keep alive

--- app/hooks/cast_sender/useGoogleCastContext.tsx
+++ app/hooks/cast_sender/useGoogleCastContext.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { type GlobalCastContext } from "./types";
 
-
 export function useGoogleCastContext() {
+  const [castContext, setCastContext] =
+    useState<cast.framework.CastSession | null>(null);
   useEffect(() => {
     const w = window as unknown as GlobalCastContext;
 
@@ -13,27 +14,61 @@ export function useGoogleCastContext() {
 
     if (w.gCastReady) {
       console.log("cast ready");
-      loadCastFramework();
+      const context = loadCastFramework();
+      context?.requestSession().then(() => {
+        setCastContext(context.getCurrentSession());
+      });
       return;
     }
 
     w["__onGCastApiAvailable"] = function (isAvailable: boolean) {
       if (isAvailable) {
         console.log("cast now available");
-        loadCastFramework();
+        const context = loadCastFramework();
+        context?.requestSession().then(() => {
+          setCastContext(context.getCurrentSession());
+        });
       }
     };
   }, []);
+
+  useEffect(() => {
+    if (!castContext) return;
+    const timeout = setInterval(() => {
+      // send dummy keepalive message to receiver
+      // https://stackoverflow.com/questions/50414540/keep-chromecast-caf-receiver-alive-after-sender-disconnect
+      const message = new chrome.cast.media.LoadRequest({
+        contentId: "https://example.com/fake/z00123.pic-o-me.jpg",
+        contentType: "image/jpeg",
+        metadata: {},
+        streamType: chrome.cast.media.StreamType.OTHER,
+      });
+      castContext
+        .loadMedia(message)
+        .then(() => {})
+        .catch(() => {
+          return;
+        })
+        .finally(() => {
+          console.log("cast receiver keepalive message sent");
+        });
+    }, 90_000);
+
+    return () => {
+      clearInterval(timeout);
+    };
+  });
 }
 
-function loadCastFramework() {
+function loadCastFramework(): cast.framework.CastContext | null {
   console.log("loading cast framework");
   const options: cast.framework.CastOptions = {
     receiverApplicationId: "9AF17368",
     autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
   };
 
-  cast.framework.CastContext.getInstance().setOptions(options);
+  const context = cast.framework.CastContext.getInstance();
+  context.setOptions(options);
 
   const remotePlayer = new cast.framework.RemotePlayer();
   const remotePlayerController = new cast.framework.RemotePlayerController(
@@ -50,4 +85,5 @@ function loadCastFramework() {
   );
 
   console.log("cast ready");
+  return context;
 }
