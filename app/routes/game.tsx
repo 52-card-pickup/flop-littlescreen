f854f19924be4f163d18eae1522eb8181a252766
michaelblawrence <michael@wondering.com>
2024-04-24 02:35:01 +0100

    extract watingroom, return promise for bet actions

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -1,15 +1,14 @@
 import { useNavigate } from "@remix-run/react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useRecoilState } from "recoil";
-import FlopButton from "~/components/FlopButton";
 import GameScreen from "~/components/GameScreen";
 import { client } from "~/flopClient";
 import { useGoogleCastContext } from "~/hooks/cast_sender/useGoogleCastContext";
 import usePlayerDetails from "~/hooks/usePlayerDetails";
 import { usePlayerPolling } from "~/hooks/usePlayerPolling";
 import { playerState } from "~/state";
-import cn from "~/utils/cn";
 import { GoogleCastButton } from "../components/GoogleCastButton";
+import { WaitingRoom } from "../components/WaitingRoom";
 
 export default function Game() {
   usePlayerPolling();
@@ -17,9 +16,7 @@ export default function Game() {
 
   const [player] = useRecoilState(playerState);
   const { playerDetails, loading } = usePlayerDetails();
-
-  const [error, setError] = useState(false);
-  const [hasErrored, setHasErrored] = useState(false);
+  const resolvers = useRef<(() => void)[]>([]);
 
   const navigate = useNavigate();
 
@@ -36,68 +33,16 @@ export default function Game() {
   }, [loading, playerDetails, navigate]);
 
   useEffect(() => {
-    if (!error) return;
-
-    let handle: ReturnType<typeof setTimeout> | null = null;
-    handle = setTimeout(() => {
-      setError(false);
-      handle = null;
-    }, 1000);
-
-    setHasErrored(true);
-
-    return () => {
-      if (handle) {
-        clearTimeout(handle);
-        setError(false);
-      }
-    };
-  }, [error]);
+    if (player.lastUpdate === null) return;
+    if (resolvers.current.length === 0) return;
+    resolvers.current.forEach((resolve) => {
+      resolve();
+    });
+    resolvers.current = [];
+  }, [player.lastUpdate]);
 
   if (showStartGameButton) {
-    return (
-      <div
-        className="min-h-screen grid grid-flow-row grid-rows-[1fr,1fr,1fr,1fr] 
-      text-white place-self-center bg-slate-200"
-      >
-        <GoogleCastButton className="fixed top-8 right-8 w-8 h-8" />
-        <div className={cn("grid justify-center items-center h-screen")}>
-          <div className={cn("flex flex-col justify-center items-center")}>
-            {hasErrored && (
-              <div className="py-10 animate-pulse">
-                <h2 className="text-black font-bold text-2xl p-2 text-center">
-                  Failed to start game
-                </h2>
-                <p className="text-black text-center">
-                  Please try again in a moment
-                </p>
-              </div>
-            )}
-            <div className={cn(error ? "animate-shake" : "")}>
-              <FlopButton
-                onClick={() => {
-                  setHasErrored(false);
-                  client
-                    .POST("/api/v1/room/close")
-                    .catch((e) => {
-                      console.error(e);
-                      return { error: e };
-                    })
-                    .then((x) => {
-                      if (x.error) {
-                        console.warn("error starting game");
-                        setError(true);
-                      }
-                    });
-                }}
-              >
-                Start Game
-              </FlopButton>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <WaitingRoom />;
   }
 
   return (
@@ -106,40 +51,84 @@ export default function Game() {
         state={player}
         actions={{
           fold: () => {
-            client.POST("/api/v1/play", {
-              body: {
-                action: "fold",
-                playerId: playerDetails.id,
-                stake: 0, // placeholder
-              },
-            });
+            return client
+              .POST("/api/v1/play", {
+                body: {
+                  action: "fold",
+                  playerId: playerDetails.id,
+                  stake: 0, // placeholder
+                },
+              })
+              .then(() => {
+                const promise = new Promise<void>((resolve) => {
+                  resolvers.current.push(resolve);
+                });
+                return promise;
+              })
+              .catch((e) => {
+                console.error(e);
+                return Promise.resolve();
+              });
           },
           raiseTo: (stake: number) => {
-            client.POST("/api/v1/play", {
-              body: {
-                action: "raiseTo",
-                stake: stake,
-                playerId: playerDetails.id,
-              },
-            });
+            return client
+              .POST("/api/v1/play", {
+                body: {
+                  action: "raiseTo",
+                  stake: stake,
+                  playerId: playerDetails.id,
+                },
+              })
+              .then(() => {
+                const promise = new Promise<void>((resolve) => {
+                  resolvers.current.push(resolve);
+                });
+                return promise;
+              })
+              .catch((e) => {
+                console.error(e);
+                return Promise.resolve();
+              });
           },
           check: () => {
-            client.POST("/api/v1/play", {
-              body: {
-                action: "check",
-                playerId: playerDetails.id,
-                stake: 0, // placeholder
-              },
-            });
+            return client
+              .POST("/api/v1/play", {
+                body: {
+                  action: "check",
+                  playerId: playerDetails.id,
+                  stake: 0, // placeholder
+                },
+              })
+              .then(() => {
+                const promise = new Promise<void>((resolve) => {
+                  resolvers.current.push(resolve);
+                });
+                return promise;
+              })
+              .catch((e) => {
+                console.error(e);
+                return Promise.resolve();
+              });
           },
           call: () => {
-            client.POST("/api/v1/play", {
-              body: {
-                action: "call",
-                playerId: playerDetails.id,
-                stake: 0, // placeholder
-              },
-            });
+            return client
+              .POST("/api/v1/play", {
+                body: {
+                  action: "call",
+                  playerId: playerDetails.id,
+                  stake: 0, // placeholder
+                },
+              })
+              .then(() => {
+                const promise = new Promise<void>((resolve) => {
+                  resolvers.current.push(resolve);
+                });
+                return promise;
+              })
+              .catch((e) => {
+                console.error(e);
+                return Promise.resolve();
+              });
           },
         }}
       />
