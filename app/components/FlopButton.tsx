eba6407b73e1a9ece367841b309e06d925b35b0b
michaelblawrence <michael@wondering.com>
2024-04-24 02:20:01 +0100

    flop button variant styling

--- app/components/FlopButton.tsx
+++ app/components/FlopButton.tsx
@@ -1,10 +1,11 @@
+import { MouseEventHandler } from "react";
 import cn from "~/utils/cn";
 
 export type ButtonColor = "gray" | "blue" | "green" | "red";
 export type ButtonVariant = "solid" | "outline";
 
 export default function FlopButton(props: {
-  onClick?: () => void;
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
   children?: React.ReactNode;
@@ -18,42 +19,44 @@ export default function FlopButton(props: {
     props.variant || "outline"
   );
   return (
-    <button
-      className="relative inline-block text-lg group disabled:opacity-50"
-      type={props.type || "button"}
-      disabled={props.disabled}
-      onClick={props.onClick}
-    >
-      <span
-        className={cn(
-          "relative z-10 block overflow-hidden font-medium leading-tight",
-          "transition-colors duration-300 ease-out border-2 rounded-lg",
-          props.slim ? "" : "px-6 py-3",
-          border,
-          text
-        )}
+    <div className={props.className} style={{ display: "flex" }}>
+      <button
+        className="relative inline-block text-lg w-full group disabled:opacity-50"
+        type={props.type || "button"}
+        disabled={props.disabled}
+        onClick={props.onClick}
       >
         <span
-          className={cn("absolute inset-0 w-full h-full px-5 py-3", bgBase)}
-        ></span>
+          className={cn(
+            "relative z-10 block overflow-hidden font-medium leading-tight",
+            "transition-colors duration-300 ease-out border-2 rounded-lg",
+            props.slim ? "" : "px-6 py-3",
+            border,
+            text
+          )}
+        >
+          <span
+            className={cn("absolute inset-0 w-full h-full px-5 py-3", bgBase)}
+          ></span>
+          <span
+            className={cn(
+              "absolute left-0 w-72 h-72 -ml-2 transition-all duration-300 origin-top-right -rotate-90",
+              "-translate-x-full translate-y-12 group-hover:-rotate-180 ease",
+              props.disabled ? "" : "group-hover:-rotate-180",
+              bg
+            )}
+          ></span>
+          <span className="relative truncate">{props.children}</span>
+        </span>
         <span
           className={cn(
-            "absolute left-0 w-72 h-72 -ml-2 transition-all duration-300 origin-top-right -rotate-90",
-            "-translate-x-full translate-y-12 ease",
-            props.disabled ? "" : "group-hover:-rotate-180",
+            "absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear",
+            "rounded-lg group-hover:mb-0 group-hover:mr-0",
             bg
           )}
         ></span>
-        <span className="relative truncate">{props.children}</span>
-      </span>
-      <span
-        className={cn(
-          "absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear",
-          "rounded-lg group-hover:mb-0 group-hover:mr-0",
-          bg
-        )}
-      ></span>
-    </button>
+      </button>
+    </div>
   );
 }
 
@@ -69,8 +72,8 @@ function colorClassNames(which: ButtonColor, variant: ButtonVariant) {
         border: "border-gray-900",
         text:
           variant === "outline"
-            ? "text-gray-800"
-            : "text-gray-50 group-hover:text-gray-900",
+            ? "text-gray-800 group-hover:text-white"
+            : "text-gray-50 group-hover:text-black",
       };
     case "blue":
       return {
@@ -82,7 +85,7 @@ function colorClassNames(which: ButtonColor, variant: ButtonVariant) {
         border: "border-blue-900",
         text:
           variant === "outline"
-            ? "text-gray-800"
+            ? "text-gray-800 group-hover:text-white"
             : "text-blue-50 group-hover:text-blue-900",
       };
     case "green":
@@ -108,7 +111,7 @@ function colorClassNames(which: ButtonColor, variant: ButtonVariant) {
         border: "border-red-900",
         text:
           variant === "outline"
-            ? "text-gray-800"
+            ? "text-gray-800 group-hover:text-white"
             : "text-red-50 group-hover:text-red-900",
       };
   }
