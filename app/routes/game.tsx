e6cbb579f1e874ab844825cdbbdb91c91b6a5170
michaelblawrence <michael@wondering.com>
2024-04-25 02:43:32 +0100

    use send button in waiting room + game screen

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -9,6 +9,8 @@ import { usePlayerPolling } from "~/hooks/usePlayerPolling";
 import { playerState } from "~/state";
 import { GoogleCastButton } from "../components/GoogleCastButton";
 import { WaitingRoom } from "../components/WaitingRoom";
+import { PlayerPhotoCamera } from "~/components/PlayerPhotoCamera";
+import PlayerSendButton from "~/components/PlayerSendButton";
 
 export default function Game() {
   usePlayerPolling();
@@ -42,97 +44,106 @@ export default function Game() {
   }, [player.lastUpdate]);
 
   if (showStartGameButton) {
-    return <WaitingRoom />;
+    return (
+      <div
+        className="fixed w-screen h-screen grid grid-flow-row grid-rows-[1fr,1fr,1fr,1fr] 
+text-white place-self-center bg-slate-200"
+      >
+        <GoogleCastButton className="fixed top-8 right-8 w-8 h-8" />
+        <WaitingRoom />
+        <div className="fixed left-0 bottom-8 w-full grid justify-center items-center">
+          <PlayerPhotoCamera className="m-8" />
+        </div>
+
+        <div className="fixed right-8 bottom-12 w-8 h-8 grid justify-center items-center z-50">
+          <PlayerSendButton />
+        </div>
+      </div>
+    );
   }
 
+  const actions = {
+    fold: async () => {
+      try {
+        await client.POST("/api/v1/play", {
+          body: {
+            action: "fold",
+            playerId: playerDetails.id,
+            stake: 0, // placeholder
+          },
+        });
+        const promise = new Promise<void>((resolve) => {
+          resolvers.current.push(resolve);
+        });
+        return await promise;
+      } catch (e) {
+        console.error(e);
+        return await Promise.resolve();
+      }
+    },
+    raiseTo: async (stake: number) => {
+      try {
+        await client.POST("/api/v1/play", {
+          body: {
+            action: "raiseTo",
+            stake: stake,
+            playerId: playerDetails.id,
+          },
+        });
+        const promise = new Promise<void>((resolve) => {
+          resolvers.current.push(resolve);
+        });
+        return await promise;
+      } catch (e) {
+        console.error(e);
+        return await Promise.resolve();
+      }
+    },
+    check: async () => {
+      try {
+        await client.POST("/api/v1/play", {
+          body: {
+            action: "check",
+            playerId: playerDetails.id,
+            stake: 0, // placeholder
+          },
+        });
+        const promise = new Promise<void>((resolve) => {
+          resolvers.current.push(resolve);
+        });
+        return await promise;
+      } catch (e) {
+        console.error(e);
+        return await Promise.resolve();
+      }
+    },
+    call: async () => {
+      try {
+        await client.POST("/api/v1/play", {
+          body: {
+            action: "call",
+            playerId: playerDetails.id,
+            stake: 0, // placeholder
+          },
+        });
+        const promise = new Promise<void>((resolve) => {
+          resolvers.current.push(resolve);
+        });
+        return await promise;
+      } catch (e) {
+        console.error(e);
+        return await Promise.resolve();
+      }
+    },
+  };
   return (
     <div className="w-screen h-screen overflow-hidden">
-      <GameScreen
-        state={player}
-        actions={{
-          fold: () => {
-            return client
-              .POST("/api/v1/play", {
-                body: {
-                  action: "fold",
-                  playerId: playerDetails.id,
-                  stake: 0, // placeholder
-                },
-              })
-              .then(() => {
-                const promise = new Promise<void>((resolve) => {
-                  resolvers.current.push(resolve);
-                });
-                return promise;
-              })
-              .catch((e) => {
-                console.error(e);
-                return Promise.resolve();
-              });
-          },
-          raiseTo: (stake: number) => {
-            return client
-              .POST("/api/v1/play", {
-                body: {
-                  action: "raiseTo",
-                  stake: stake,
-                  playerId: playerDetails.id,
-                },
-              })
-              .then(() => {
-                const promise = new Promise<void>((resolve) => {
-                  resolvers.current.push(resolve);
-                });
-                return promise;
-              })
-              .catch((e) => {
-                console.error(e);
-                return Promise.resolve();
-              });
-          },
-          check: () => {
-            return client
-              .POST("/api/v1/play", {
-                body: {
-                  action: "check",
-                  playerId: playerDetails.id,
-                  stake: 0, // placeholder
-                },
-              })
-              .then(() => {
-                const promise = new Promise<void>((resolve) => {
-                  resolvers.current.push(resolve);
-                });
-                return promise;
-              })
-              .catch((e) => {
-                console.error(e);
-                return Promise.resolve();
-              });
-          },
-          call: () => {
-            return client
-              .POST("/api/v1/play", {
-                body: {
-                  action: "call",
-                  playerId: playerDetails.id,
-                  stake: 0, // placeholder
-                },
-              })
-              .then(() => {
-                const promise = new Promise<void>((resolve) => {
-                  resolvers.current.push(resolve);
-                });
-                return promise;
-              })
-              .catch((e) => {
-                console.error(e);
-                return Promise.resolve();
-              });
-          },
-        }}
-      />
+      <GameScreen state={player} actions={actions} />
       <GoogleCastButton className="fixed top-8 right-8 w-8 h-8" />
+
+      <div className="fixed right-8 bottom-12 w-8 h-8 grid justify-center items-center z-50">
+        <PlayerSendButton />
+      </div>
     </div>
   );
 }
