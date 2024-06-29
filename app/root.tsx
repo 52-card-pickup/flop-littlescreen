1ceb0034c07c0ff6b86d450f9571aa905df890ae
michaelblawrence <michael@wondering.com>
2024-06-29 12:32:40 +0100

    tweak title bar

--- app/root.tsx
+++ app/root.tsx
@@ -35,7 +35,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>flop - the mobile game</title>
+        <title>flop. poker - the mobile game</title>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
