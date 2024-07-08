29265af94e948c784993c8249f564560c9fb5269
michaelblawrence <michael@wondering.com>
2024-07-08 23:56:57 +0100

    slight send menu transparency

--- app/components/PlayerSendButton.tsx
+++ app/components/PlayerSendButton.tsx
@@ -73,7 +73,10 @@ export default function PlayerSendButton() {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute bottom-14 right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-300 rounded-xl bg-white shadow-lg ring-1 ring-black ring-opacity-30 overflow-hidden focus:outline-none">
+          <Menu.Items
+            className="absolute bottom-14 right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-300
+          rounded-xl bg-white shadow-lg ring-1 ring-black ring-opacity-30 overflow-hidden opacity-95 focus:outline-none"
+          >
             <div className="py-1">
               <Menu.Item>
                 <a
