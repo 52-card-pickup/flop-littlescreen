c0402834cdb89abb4ff0f9a1bf7cb04e24b439c8
michaelblawrence <michael@wondering.com>
2024-10-09 00:19:37 +0100

    use router outlet

--- app/root.tsx
+++ app/root.tsx
@@ -57,7 +57,7 @@ export default function App() {
       <AppBody>
         <RecoilRoot>
           <ToasterProvider>
-            <Index />
+            <Outlet />
           </ToasterProvider>
         </RecoilRoot>
         <ScrollRestoration />
