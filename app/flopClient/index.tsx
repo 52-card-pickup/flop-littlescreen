6e613b60cb748b164b2a609b5f2ee239f234bc1e
michaelblawrence <michael@wondering.com>
2024-10-20 23:56:13 +0100

    init client with env for local debugging

--- app/flopClient/index.tsx
+++ app/flopClient/index.tsx
@@ -1,6 +1,68 @@
 import type { paths } from "~/flopClient/spec";
 import createClient from "openapi-fetch";
 
-export const client = createClient<paths>({
-  baseUrl: import.meta.env.DEV ? "http://localhost:8080/" : "/",
-});
+type FlopClientState = {
+  client: ReturnType<typeof createClient<paths>>;
+  setClient: (client: ReturnType<typeof createClient<paths>>) => void;
+  isInitialized: () => boolean;
+};
+
+const state = init();
+
+function init(): FlopClientState {
+  let resolve: (() => void) | undefined = undefined;
+  const promise = new Promise<void>((r) => {
+    resolve = r;
+  });
+
+  let clientState: ReturnType<typeof createClient<paths>> | null = null;
+
+  const flopClientState: FlopClientState = {
+    setClient(client: ReturnType<typeof createClient<paths>>) {
+      clientState = client;
+      resolve?.();
+      resolve = undefined;
+    },
+    client: clientState!,
+    isInitialized: () => !!clientState,
+  };
+
+  const clientProxy = new Proxy({} as ReturnType<typeof createClient<paths>>, {
+    get(target, prop) {
+      if (!clientState) {
+        return async (...args: any[]) => {
+          await promise;
+          if (!clientState) {
+            throw new Error("Client not initialized");
+          }
+
+          return Reflect.get(clientState, prop)(...args);
+        };
+      }
+
+      return Reflect.get(clientState, prop);
+    },
+    set() {
+      throw new Error("Client cannot be modified");
+    },
+  });
+
+  flopClientState.client = clientProxy;
+  return flopClientState;
+}
+
+export function tryInitializeClient(
+  env: { API_URL: string | null } = globalThis.ENV
+) {
+  if (state.isInitialized()) {
+    return;
+  }
+
+  const client = createClient<paths>({
+    baseUrl: env?.API_URL ?? undefined,
+  });
+
+  state.setClient(client);
+}
+
+export const client = state.client;
